a623d455a0b07281f52926418bc5b9b244c6905c
imre-gabor <gabor@aut.bme.hu>
2023-11-02 13:12:08 +0100

    transform model classes to be JPA entities

--- webshop_en/src/main/java/webshop/model/Product.java
+++ webshop_en/src/main/java/webshop/model/Product.java
@@ -1,15 +1,31 @@
 package webshop.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(of = "id")
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
 
+	@Id
+	@GeneratedValue
 	private int id;
 	private String name;
 	private double price;
 	
+	@ManyToOne
 	private Category category;
+	
+	
 }
